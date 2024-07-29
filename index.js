52795743f79fc0b2cbc29458993e2b98f2d7aa33
LuizFAraujo <luizfaraujo.eng@gmail.com>
2024-07-29 01:15:55 -0300

    Adicionado MongoDB, Configuração Inicial Ok.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import pkg from "pg";
+import { MongoClient } from "mongodb";
+
 const { Pool } = pkg;
 
 dotenv.config(); // Carregar variáveis de ambiente do arquivo .env
 
 // Configuração do pool de conexão com o PostgreSQL
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const dbSql = new Pool({ connectionString: process.env.SQL_URL });
+
+// Configuração do cliente MongoDB
+const dbNoSql = new MongoClient(process.env.NOSQL_URL);
 
 const app = express();
 app.use(express.json()); // Configurar o middleware para processar JSON
@@ -16,11 +21,13 @@ app.get("/", (req, res) => {
   res.send("API funcionando!");
 });
 
-// Conectar ao banco de dados e iniciar o servidor
-pool
-  .connect()
+// Conectar ao banco de dados PostgreSQL e MongoDB e iniciar o servidor
+Promise.all([
+  dbSql.connect(),
+  dbNoSql.connect()
+])
   .then(() => {
-    console.log("Conectado ao banco de dados PostgreSQL...");
+    console.log("Conectado ao banco de dados PostgreSQL e MongoDB...");
 
     // Iniciar o servidor na porta 3000
     app.listen(3000, () => {
@@ -28,12 +35,18 @@ pool
     });
   })
   .catch((err) => {
-    console.error("Erro ao conectar com o banco de dados PostgreSQL: ", err);
+    console.error("Erro ao conectar com o banco de dados: ", err);
     process.exit(-1);
   });
 
 // Configurar o evento de erro para o pool de conexões
-pool.on("error", (err) => {
-  console.error("Erro inesperado: ", err);
+dbSql.on("error", (err) => {
+  console.error("Banco SQL. Erro inesperado: ", err);
+  process.exit(-1);
+});
+
+// Configurar o evento de erro para o cliente MongoDB
+dbNoSql.on("error", (err) => {
+  console.error("Banco NoSQL. Erro inesperado: ", err);
   process.exit(-1);
 });
